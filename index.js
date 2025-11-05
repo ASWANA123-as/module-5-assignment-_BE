920ea2b497acb28f672526affe2b774a603dbe1e
Aswana <aswanaanu201@gmail.com>
2025-11-05 23:39:09 +0530

    commit

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ const connectDb=require('./config/db')
 const userroute=require('./Route/userRoute')
 const taskRoute=require('./Route/taskRoute')
 connectDb()
-app.use(cors({
-  origin: [
-    "https://module-5-assignment-fe-ouek.vercel.app",
-    "http://localhost:5173"
-  ],
-  credentials: true
-}));
+const allowedOrigins = [
+  "https://module-5-assignment-fe-hbfc.vercel.app", // ✅ your live frontend
+  "http://localhost:5173" // ✅ for local dev
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use(cookieParser());
 app.use(express.json())
